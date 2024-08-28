ee13e32dd1122ec220209109592f0ae27d4dad92
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-28 20:01:23 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Task 0 -> Regex-ing """
+""" Tasks -> Regex-ing """
+import logging
 import re
 from typing import List
 
@@ -14,3 +15,22 @@ def filter_datum(fields: List[str],
                   match.group(1),
                   redaction),
                   message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "*"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init the RedactingFormatter with the given fields """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record to redact sensitive data """
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
