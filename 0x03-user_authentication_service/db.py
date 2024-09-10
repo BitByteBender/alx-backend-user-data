933213d6f88c8171d0e56e385a38ae05fb96194d
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-10 10:37:58 +0100

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import Base, User
 
 
@@ -43,3 +43,13 @@ class DB:
             self._session.rollback()
             new_usr = None
         return new_usr
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Method that finds a user by arbitrary keyword args """
+        try:
+            usr = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("User does not exist!")
+        except InvalidRequestError:
+            raise InvalidRequestError("Wrong query args are passed")
+        return usr
