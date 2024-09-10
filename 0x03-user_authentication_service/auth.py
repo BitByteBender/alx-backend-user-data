aa146d2ad8ca4994ea829462c41e1ba74e201b89
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-10 18:37:33 +0100

    New test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,6 +26,8 @@ class Auth:
         """ Inserts the newly created user into the db """
         try:
             self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        raise ValueError("User {} already exists".format(email))
+        except InvalidRequestError:
+            raise ValueError("Invalid query args")
