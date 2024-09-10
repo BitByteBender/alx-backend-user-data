0e9d1ef4021edbe4251c01be9800438d6bf5eb91
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-10 10:49:50 +0100

    User creation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -24,7 +23,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -32,25 +31,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """Method that adds a newly created user
         """
-            Method that adds a new user to the db
-            Returns a newly created user obj
-        """
-        try:
-            new_usr = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_usr)
-            self._session.commit()
-        except Exception as e:
-            self._session.rollback()
-            new_usr = None
-        return new_usr
-
-    def find_user_by(self, **kwargs) -> User:
-        """ Method that finds a user by arbitrary keyword args """
-        try:
-            usr = self._session.query(User).filter_by(**kwargs).one()
-        except NoResultFound:
-            raise NoResultFound("User does not exist!")
-        except InvalidRequestError:
-            raise InvalidRequestError("Wrong query args are passed")
-        return usr
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
