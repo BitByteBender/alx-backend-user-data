5b50d99137640c1f2a8a27f70d30989f07e2d289
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-10 20:23:52 +0100

    Req check

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,7 +45,8 @@ def login() -> str:
     email = request.form.get("email")
     password = request.form.get("password")
 
-    if not AUTH.valid_login(email, password):
+    valid_user = AUTH.valid_login(email, password)
+    if not valid_user:
         abort(401)
 
     session_id = AUTH.create_session(email)
