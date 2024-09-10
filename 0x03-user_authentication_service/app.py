ef4619f62b1b91bab158c5d8544503152ef9a5fd
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-10 22:26:39 +0100

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,5 +68,15 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """Loads the user profile
+    """
+    session_id = request.cookies.get("session_id")
+    if AUTH.get_user_from_session_id(session_id) is None:
+        abort(403)
+    return jsonify({"email": AUTH.get_user_from_session_id(session_id).email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
